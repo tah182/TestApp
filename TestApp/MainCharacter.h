14b12064f2d4b89c132b1b3a6aeec9fd9c691901
Tah Tatsumoto <tah1@Tahs-Macbook.local>
2014-06-10 20:59:46 -0600

    Test

---
+++ TestApp/MainCharacter.h
@@ -0,0 +1,13 @@
+//
+//  MainCharacter.h
+//  TestApp
+//
+//  Created by Tah Tatsumoto on 6/10/14.
+//  Copyright (c) 2014 Project Umbrella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainCharacter : NSObject
+
+@end
---
+++ TestApp/MainCharacter.m
@@ -0,0 +1,25 @@
+//
+//  MainCharacter.m
+//  TestApp
+//
+//  Created by Tah Tatsumoto on 6/4/14.
+//  Copyright (c) 2014 Project Umbrella. All rights reserved.
+//
+
+#import "MainCharacter.h"
+
+@implementation MainCharacter
+-(void) MainCharacter {
+    
+}
+
+//struct BaseCharacteristics {
+//    BOOL male;
+//    Dictionary *weapons = @ {
+//        @"./images/hero/fist" : [NSNumber numberWithInt: 0]
+//    };
+//};
+//
+//struct
+
+@end
