f1aa79f9c57126f876849ea78c4f1be1590bae81
Tah Tatsumoto <tah1@Tahs-Macbook.local>
2014-06-10 20:24:59 -0600

    Initial Commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Tah Tatsumoto on 6/10/14.
+//  Copyright (c) 2014 Project Umbrella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestApp/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  TestApp
+//
+
+//  Copyright (c) 2014 Project Umbrella. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
