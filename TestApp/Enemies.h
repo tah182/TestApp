02f1825627bd9d5e900896ac5b72be8aa8d50483
Nathan Knight <nathanknight@Nathans-MacBook.local>
2014-06-10 21:27:29 -0600

    Nate’s test of new file

---
+++ TestApp/Enemies.h
@@ -0,0 +1,13 @@
+//
+//  Enemies.h
+//  TestApp
+//
+//  Created by Nathan Knight on 6/10/14.
+//  Copyright (c) 2014 Project Umbrella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Enemies : NSObject
+
+@end
---
+++ TestApp/Enemies.m
@@ -0,0 +1,13 @@
+//
+//  Enemies.m
+//  TestApp
+//
+//  Created by Nathan Knight on 6/10/14.
+//  Copyright (c) 2014 Project Umbrella. All rights reserved.
+//
+
+#import "Enemies.h"
+
+@implementation Enemies
+
+@end
